6fdc40bc60fb71e40ae7a8109ff5151bdd007f27
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-04-18 22:10:28 +0600

    Learning Advanced Data Fetching

---
+++ src/app/posts/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostsPage = () => {
+    return (
+        <div>
+            <h2>Posts are coming soon...</h2>
+        </div>
+    );
+};
+
+export default PostsPage;
